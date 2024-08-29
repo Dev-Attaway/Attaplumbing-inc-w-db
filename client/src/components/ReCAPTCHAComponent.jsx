63e1ced7aa872e994971962ff209642460947877
Nick <145059598+Dev-Attaway@users.noreply.github.com>
2024-08-29 15:23:21 -0700

    Update ReCAPTCHAComponent.jsx

--- client/src/components/ReCAPTCHAComponent.jsx
+++ client/src/components/ReCAPTCHAComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useImperativeHandle, forwardRef } from "react";
+import { useImperativeHandle, forwardRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
 // ForwardRef to pass ref to the parent component
